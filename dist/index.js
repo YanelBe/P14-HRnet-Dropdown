6821caa31d406b0e7434ff1a087634f432c30518
YanelBe <yanel.bensa@gmail.com>
2024-03-08 17:11:25 +0100

    Transpile correction

--- dist/index.js
+++ dist/index.js
@@ -4,6 +4,6 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports["default"] = void 0;
-var _Dropdown = _interopRequireDefault(require("./Dropdown.jsx"));
+var _Dropdown = _interopRequireDefault(require("./Dropdown"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 var _default = exports["default"] = _Dropdown["default"];
